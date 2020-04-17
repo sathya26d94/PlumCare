777d772c4e13beb9cf1ad015882d844101039e91
sathya26d <sathiyamoorthy@theviralfever.com>
2020-04-17 17:22:42 +0530

    Refractored the code, includes bottom tab bar

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {StyleSheet, Platform, Image, Text, View} from 'react-native';
 import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 
-import Loading from './Loading';
-import SignUp from './SignUp';
-import Login from './Login';
-import Main from './Main';
+import Loading from './Components/Loading';
+import SignUp from './Components/SignUp';
+import Login from './Components/Login';
+import Main from './Components/Main';
 
 const App = createAppContainer(
   createSwitchNavigator(
--- Components/Main.js
+++ Components/Main.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import Home from './Home';
-import Profile from './Profile';
+import Profile from '../Profile';
 
 const Main = createBottomTabNavigator({
   Home: Home,
